d786fb99f616c8700790ba940fea0fab6d031a0a
brunobandelli <brunobandelli@gmail.com>
2021-04-04 23:24:38 -0300

    BOTAO EDIT
    DESENVOLVENDO O BOTÃO EDIT

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -15,7 +15,7 @@ router.post("/categories/save", (req, res) =>{                  //ROTA: /categor
             title: title,                                       //RECEBE A VARIAVEL title DO FORMULÁRIO (new.ejs)
             slug: slugify(title)                                //OTIMIZAÇÃO DE TITULO PARA ROTA
         }).then (() => {                                        //SE DER CERTO
-            res.redirect("/");                                  //REDIRECIONA PARA ROTA PRINCIPAL
+            res.redirect("/admin/categories");                                  //REDIRECIONA PARA ROTA PRINCIPAL
         })
 
 
@@ -50,5 +50,26 @@ router.post("/categories/delete", (req, res) =>{                    //ROTA:/cate
         res.redirect("/admin/categories");                          //VOU REDIRECIONAR PARA: /admin/categories
     }
 })
+
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+    var id = req.params.id;                                         //VOU RECEBER UM ID QUE VAI VIR DOS PARAMETROS DA MINHA ROTA
+    if(isNaN(id)){                                                  //CORREÇAO DO BUG DA ROTA
+        res.redirect("/admin/categories")
+    }
+    Category.findByPk(id).then(category => {                        //MODEL Categody // findByPk() METODO DE PESQUISA //then SE DER CERTO, A CATEGORIA FOR ACHADA
+        if(category != undefined) {                                 //SE FOR DIFERENTE DE UNDEFINED
+            res.render("admin/categories/edit",{                    //RENDERIZA A VIEW: edit.ejs DENTRO DA PASTA(views/admin/categories)
+                category: category                                  //A VAR categoria: VAI RECEBER OS DADOS DA categoria, NO QUAL SERÁ PASSADO ESSAS DADOS PARA A VIEW NO FRONT END.
+            });
+        }else{                                                      //SE NÃO FOR DIFERENTE DE UNDEFINED
+            res.redirect("/admin/categories");
+        }
+    }).catch(erro => {                                              //SE DER ERRO, NÃO ACHAR A CATEGORIA
+        res.redirect("/admin/categories");
+    })
+})
+
+
 
 module.exports = router;                                        //EXPORTAÇÃO
