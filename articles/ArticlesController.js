3fa67bcff47d5ac9465e4b09ec2912f3ac3be484
brunobandelli <brunobandelli@gmail.com>
2021-04-08 22:53:13 -0300

    Save de formulários

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,34 @@
 const express = require("express");                           //EXPRESS
 const router = express.Router();                              //ROUTER
+const Category = require("../categories/Category")            //IMPORTANDO MODEL Category  
+const Article = require("./Article");                         //MODEL: Article
+const slugify = require("slugify");                           //BIBLIOTECA DO SLUGIFY
 
-router.get("/articles",(req, res) => {                        //ROTA: /articles
+router.get("/admin/articles",(req, res) => {                        //ROTA: /articles
     res.send("ROTA DE ARTIGOS")
 });
 
 router.get("/admin/articles/new", (req, res) => {             //ROTA: /admin/articles/new
-    res.render("admin/articles/new")
+    Category.findAll().then(categories => {                   //RECEBE TODAS AS CATEGORIAS
+        res.render("admin/articles/new",{                     //RENDERIZA A NEW
+            categories: categories                            //PASSA AS CATEGORIAS PARA A VARIAVEL CATEGORIAS QUE SERÁ UTILIZADA NO FRONTEND.
+        })
+    })
 })
 
+router.post("/articles/save",(req, res) => {
+    var title = req.body.title;                              // CORPO DO CAMPO NAME TITLE DO HTML 
+    var body = req.body.body;                                // CORPO DO CAMPO NAME BODY DO HTML
+    var category = req.body.category;                        // CORPO DO CAMPO NAME CATEGORY DO HTML
+
+    Article.create({                                        // MODEL (Article.js) ADD UMA NOVA LINHA NO BANCO DE DADOS
+        title: title,                                       // RECEBE O VALOR DA VARIAVEL title DO FORMULÁRIO (new.ejs)
+        slug: slugify(title),                               // ATUALIZARÁ O SLUG CONFORME O NOVO TITULO
+        body: body,                                         // RECEBE O VALOR DA VARIAVEL body DO FORMULÁRIO (new.ejs)
+        categoryId: category,                               // REBE O ID DA CATEGORIA
+        
+    }).then(() => {
+        res.redirect("/admin/articles");
+    });
+});
 module.exports = router;                                      //EXPORTAÇÃO
--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");                                     //Bi
 
 const connection = new Sequelize('guiapress','root','mysqlbn94',{           //COMUNICAÇÃO COM O SERVIDOR
     host: 'localhost',                                                      //LOCALIZAÇÃO DO SERVIDOR
-    dialect: 'mysql'                                                        //TIPO DE BANCO DE DADOS
+    dialect: 'mysql',                                                        //TIPO DE BANCO DE DADOS
+    timezone: "-03:00",                                                     //CONFIGURAÇÃO DE HORAS DO SEQUELIZE PARA HORA DO BRASIL
 });
 
 module.exports = connection;                                                //MODULO PARA EXPORTAÇÃO DA CONEXÃO PARA OS OUTROS ARQUIVOS
