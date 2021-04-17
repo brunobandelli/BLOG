554d96a5bb09a7a6274ada7f0765818becf9f4af
brunobandelli <brunobandelli@gmail.com>
2021-04-16 22:42:58 -0300

    edit artigo

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -74,4 +74,27 @@ router.get("/admin/articles/edit/:id",(req, res) => {
     });
 })
 
+router.post("/articles/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;                                           //
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.update({
+        title: title,
+        body: body,
+        categoryId: category,
+        slug: slugify(title)
+    },{
+        where: {
+            id: id
+            
+        }
+    }).then(() => {
+        res.redirect("admin/articles");
+    }).catch(err => {
+        res.redirect("/");
+    });
+});
+
 module.exports = router;                                      //EXPORTAÇÃO
