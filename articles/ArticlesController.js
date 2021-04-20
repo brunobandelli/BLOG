183bc82101cfc1fbaa3ea2ee350452a508357906
brunobandelli <brunobandelli@gmail.com>
2021-04-19 22:55:32 -0300

    logica de paginação

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -97,4 +97,34 @@ router.post("/articles/update", (req, res) => {
     });
 });
 
+router.get("/articles/page/:num", (req, res) => {
+    var page = req.params.num;
+    var offset = 0;
+    if(isNaN(page) || page <= 1 || page == 0) {
+        offset = 0;
+
+    }else{
+        offset = (parseInt(page)-1) * 4;
+    }
+    Article.findAndCountAll({
+        limit: 4,
+        offset: offset,
+        order: [['id', 'DESC']]
+    }).then(articles => {
+        
+        var next;
+        if(offset + 4 >= articles.count){
+            next = false;
+        }else{
+            next = true;
+        }
+
+        var result = {
+            next: next,
+            articles: articles
+        }
+        res.json(result);
+    })
+})
+
 module.exports = router;                                      //EXPORTAÇÃO
