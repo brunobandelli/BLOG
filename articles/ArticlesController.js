3e924e449e9f0eb02ae003e4918d23311159f151
brunobandelli <brunobandelli@gmail.com>
2021-04-19 23:20:22 -0300

    PAGINAÇÃO NO FRONT END

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -123,7 +123,13 @@ router.get("/articles/page/:num", (req, res) => {
             next: next,
             articles: articles
         }
-        res.json(result);
+
+        Category.findAll().then(categories => {
+            res.render("admin/articles/page",{
+                result: result,
+                categories: categories
+            })
+        })
     })
 })
 
